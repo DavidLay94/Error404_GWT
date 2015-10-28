fce4ac2b656feba6ab339eb9b68ce5e64aab9dcc
rexorci <christoph.t.weber@gmail.com>
2015-10-28 21:26:52 +0100

    grundgerüst gebaut

--- Error404/src/movie/db/client/Error404.java
+++ Error404/src/movie/db/client/Error404.java
@@ -4,6 +4,8 @@ package movie.db.client;
 /* a small step for software engineering, a huge step for error404! */
 //blablablatestestest ich bin ned kreativ
 import movie.db.shared.FieldVerifier;
+
+import com.google.gwt.aria.client.ListboxRole;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -14,8 +16,11 @@ import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -28,123 +33,75 @@ public class Error404 implements EntryPoint {
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
 	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network " + "connection and try again.";
-
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
 
 	/**
 	 * This is the entry point method.
 	 */
+	private VerticalPanel mainPanel = new VerticalPanel();
+	
+	private HorizontalPanel selectionPanel = new HorizontalPanel();
+	private HorizontalPanel timebarPanel = new HorizontalPanel();
+	private HorizontalPanel worldmapPanel = new HorizontalPanel();
+	
+	private FlexTable selectionCriteriaTable = new FlexTable();
+	private Label genreLabel = new Label(); 
+	private Label langLabel = new Label(); 
+	private Label formatLabel = new Label(); 
+	private Button showButton = new Button();
+	
+	private ListBox genreDropdown = new ListBox();
+	private ListBox langDropdown = new ListBox();
+	private ListBox formatDropdown = new ListBox();
+	
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		genreLabel.setText("Genre:");
+		langLabel.setText("Language:");
+		formatLabel.setText("Format:");
+		showButton.setText("Show Results");
+		
+		genreDropdown.addItem("Romance");	
+		genreDropdown.addItem("Comedy");
+		genreDropdown.addItem("Action");
+		genreDropdown.addItem("Horror");
+		genreDropdown.setVisibleItemCount(1);
+		
+		langDropdown.addItem("English");	
+		langDropdown.addItem("German");	
+		langDropdown.addItem("Suahili");	
+		langDropdown.addItem("Klingonian");	
+		langDropdown.setVisibleItemCount(1);
+		
+		formatDropdown.addItem("Heatmap");	
+		formatDropdown.addItem("Piechart");	
+		formatDropdown.addItem("Barchart");	
+		formatDropdown.setVisibleItemCount(1);
+		
+		selectionCriteriaTable.setWidget(0,0,genreLabel);
+		selectionCriteriaTable.setWidget(0,1,genreDropdown);
+		selectionCriteriaTable.setWidget(1,0,langLabel);
+		selectionCriteriaTable.setWidget(1,1,langDropdown);
+		selectionCriteriaTable.setWidget(2,0,formatLabel);
+		selectionCriteriaTable.setWidget(2,1,formatDropdown);
+		selectionCriteriaTable.setWidget(3,0,showButton);
+		
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+		
+		selectionPanel.add(selectionCriteriaTable);
+		
+		timebarPanel.setWidth("1000px");
+		timebarPanel.setHeight("50px");
+		timebarPanel.setBorderWidth(3);
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox.setText("Remote Procedure Call - Failure");
-						serverResponseLabel.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(SERVER_ERROR);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-
-					public void onSuccess(String result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel.removeStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(result);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-				});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		worldmapPanel.setWidth("1000px");
+		worldmapPanel.setHeight("500px");
+		worldmapPanel.setBorderWidth(3);		
+		
+		mainPanel.add(selectionPanel);
+		mainPanel.add(timebarPanel);
+		mainPanel.add(worldmapPanel);
+		
+		// Associate the Main panel with the HTML host page.
+		RootPanel.get("mainPage").add(mainPanel);
 	}
 }
 
