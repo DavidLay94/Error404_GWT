208730849df9552b72e2dcf7bf7760f51205a510
rexorci <christoph.t.weber@gmail.com>
2015-11-01 19:22:25 +0100

    prepared selection data for easy input in sql-query

--- Error404/src/movie/db/client/Error404.java
+++ Error404/src/movie/db/client/Error404.java
@@ -7,6 +7,8 @@ package movie.db.client;
 
 
 
+import java.util.ArrayList;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
@@ -17,6 +19,7 @@ import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RadioButton;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -37,8 +40,20 @@ public class Error404 implements EntryPoint {
 	public void onModuleLoad() {
 		initializePanels();
 	}
+	
+	/* Class Variables */
 	private VerticalPanel mainPanel = new VerticalPanel();
-	private TextBox dummyTextBox = new TextBox();
+	private TextArea dummyTextArea = new TextArea();
+	private RadioButton genreRBAnd = new RadioButton("genreRBGroup", "AND");
+	private RadioButton genreRBOR = new RadioButton("genreRBGroup", "OR");
+	private RadioButton countryRBAnd = new RadioButton("countryRBGroup", "AND");
+	private RadioButton countryRBOR = new RadioButton("countryRBGroup", "OR");
+	private RadioButton langRBAnd = new RadioButton("langRBGroup", "AND");
+	private RadioButton langRBOR = new RadioButton("langRBGroup", "OR");
+	private ListBox genreListBox = new ListBox();		
+	private ListBox countryListBox = new ListBox();
+	private ListBox langListBox = new ListBox();
+	
 	private void initializePanels(){
 		
 		HorizontalPanel selectionPanel = new HorizontalPanel();
@@ -53,53 +68,38 @@ public class Error404 implements EntryPoint {
 			}
 		});
 		
-		ListBox genreDropdown = new ListBox();
-		
-		ListBox countryDropdown = new ListBox();
-		ListBox langDropdown = new ListBox();
-		ListBox formatDropdown = new ListBox();
-				
 		showAsButton.setText("Show as");		
-		genreDropdown.setMultipleSelect(true);
-		initializeGenres(genreDropdown);
-		initializeCountries(countryDropdown);
-		initializeLanguages(langDropdown);	
+		initializeGenres();
+		initializeCountries();
+		initializeLanguages();	
 
-		RadioButton genreRBAnd = new RadioButton("genreRBGroup", "AND");
-		genreRBAnd.setValue(true);
-		RadioButton genreRBOR = new RadioButton("genreRBGroup", "OR");
-		
-		RadioButton countryRBAnd = new RadioButton("countryRBGroup", "AND");
-		countryRBAnd.setValue(true);
-		RadioButton countryRBOR = new RadioButton("countryRBGroup", "OR");
-		
-		RadioButton langRBAnd = new RadioButton("langRBGroup", "AND");
+		genreRBAnd.setValue(true);		
+		countryRBAnd.setValue(true);		
 		langRBAnd.setValue(true);
-		RadioButton langRBOR = new RadioButton("langRBGroup", "OR");
 		
 		selectionCriteriaTable.setText(0,0,"Genre:");
-		selectionCriteriaTable.setWidget(0,1,genreDropdown);
+		selectionCriteriaTable.setWidget(0,1,genreListBox);
 		selectionCriteriaTable.setWidget(0,2,genreRBAnd);
 		selectionCriteriaTable.setWidget(0,3,genreRBOR);
 		selectionCriteriaTable.setText(1,0,"Country:");
-		selectionCriteriaTable.setWidget(1,1,countryDropdown);
+		selectionCriteriaTable.setWidget(1,1,countryListBox);
 		selectionCriteriaTable.setWidget(1,2,countryRBAnd);
 		selectionCriteriaTable.setWidget(1,3,countryRBOR);
 		selectionCriteriaTable.setText(2,0,"Language:");
-		selectionCriteriaTable.setWidget(2,1,langDropdown);
+		selectionCriteriaTable.setWidget(2,1,langListBox);
 		selectionCriteriaTable.setWidget(2,2,langRBAnd);
 		selectionCriteriaTable.setWidget(2,3,langRBOR);
 		selectionCriteriaTable.setWidget(3,0,showAsButton);
-		selectionCriteriaTable.setWidget(3,1,initializeFormats(formatDropdown));
+		selectionCriteriaTable.setWidget(3,1,initializeFormats());
 			
 		
 		selectionPanel.add(selectionCriteriaTable);
 		
-		timebarPanel.setWidth("1000px");
-		timebarPanel.setHeight("50px");
+		timebarPanel.setWidth("1500px");
+		timebarPanel.setHeight("250px");
 		timebarPanel.setBorderWidth(3);
 
-		worldmapPanel.setWidth("1000px");
+		worldmapPanel.setWidth("1500px");
 		worldmapPanel.setHeight("500px");
 		worldmapPanel.setBorderWidth(3);		
 		
@@ -111,25 +111,31 @@ public class Error404 implements EntryPoint {
 		RootPanel.get("mainPage").add(mainPanel);
 	}
 	
-	private void initializeGenres(ListBox genreDropdown){		
+	private void initializeGenres(){		
 		for(Genres genre : Genres.values()){
-			genreDropdown.addItem(genre.getName());
+			genreListBox.addItem(genre.getName());
 		}
-		genreDropdown.setVisibleItemCount(5);
+		genreListBox.setVisibleItemCount(5);
+		genreListBox.setMultipleSelect(true);
+		genreListBox.setItemSelected(0, false); //first item is selected by default
 	}
-	private void initializeCountries(ListBox countryDropdown){
+	private void initializeCountries(){
 		for(Countries country : Countries.values()){
-			countryDropdown.addItem(country.getName());
+			countryListBox.addItem(country.getName());
 		}	
-		countryDropdown.setVisibleItemCount(5);
+		countryListBox.setVisibleItemCount(5);
+		countryListBox.setMultipleSelect(true);
+		countryListBox.setItemSelected(0, false); //first item is selected by default
 	}
-	private void initializeLanguages(ListBox langDropdown){
+	private void initializeLanguages(){
 		for(Languages language : Languages.values()){
-			langDropdown.addItem(language.getName());
+			langListBox.addItem(language.getName());
 		}
-		langDropdown.setVisibleItemCount(5);
+		langListBox.setVisibleItemCount(5);
+		langListBox.setMultipleSelect(true);
+		langListBox.setItemSelected(0, false); //first item is selected by default
 	}
-	private VerticalPanel initializeFormats(ListBox formatDropdown){
+	private VerticalPanel initializeFormats(){
 		VerticalPanel formatsPanel = new VerticalPanel();
 		for(Formats format : Formats.values()){
 			formatsPanel.add(new RadioButton("formatsRBGroup", format.getName()));
@@ -138,9 +144,71 @@ public class Error404 implements EntryPoint {
 		return formatsPanel;
 	}	
 	private final void showAsButtonClick(){
-		dummyTextBox.setText("Random: " + Random.nextInt());
-		((HorizontalPanel)mainPanel.getWidget(1)).add(dummyTextBox);
+
 		
+		dummyTextArea.setWidth("500px");
+		dummyTextArea.setHeight("200px");
+		ArrayList<String> selectedGenres = new ArrayList<String>();
+		if(genreListBox.getSelectedIndex() != -1){		
+			for(int i = 0;i< genreListBox.getItemCount();i++){
+				if(genreListBox.isItemSelected(i)){
+					selectedGenres.add(genreListBox.getItemText(i));
+				}
+			}
+		}
+		ArrayList<String> selectedCountries = new ArrayList<String>();
+		if(countryListBox.getSelectedIndex() != -1){		
+			for(int i = 0;i< countryListBox.getItemCount();i++){
+				if(countryListBox.isItemSelected(i)){
+					selectedCountries.add(countryListBox.getItemText(i));
+				}
+			}
+		}
+		ArrayList<String> selectedLanguages = new ArrayList<String>();
+		if(langListBox.getSelectedIndex() != -1){		
+			for(int i = 0;i< langListBox.getItemCount();i++){
+				if(langListBox.isItemSelected(i)){
+					selectedLanguages.add(langListBox.getItemText(i));
+				}
+			}
+		}
+		
+		
+		String dummyTextAreaString = "Genres:\n";
+		for(String selected : selectedGenres){
+			dummyTextAreaString = dummyTextAreaString + selected + "\n";			
+		}
+		dummyTextAreaString = dummyTextAreaString + "Connection: ";
+		if(genreRBAnd.getValue()){
+			dummyTextAreaString = dummyTextAreaString + "AND";
+		}else{
+			dummyTextAreaString = dummyTextAreaString + "OR";
+		}
+		
+		dummyTextAreaString = dummyTextAreaString + "\n\nCountries:\n";
+		for(String selected : selectedCountries){
+			dummyTextAreaString = dummyTextAreaString + selected + "\n";			
+		}
+		dummyTextAreaString = dummyTextAreaString + "Connection: ";
+		if(countryRBAnd.getValue()){
+			dummyTextAreaString = dummyTextAreaString + "AND";
+		}else{
+			dummyTextAreaString = dummyTextAreaString + "OR";
+		}
+		
+		dummyTextAreaString = dummyTextAreaString + "\n\nLanguages:\n";
+		for(String selected : selectedLanguages){
+			dummyTextAreaString = dummyTextAreaString + selected  + "\n";			
+		}
+		dummyTextAreaString = dummyTextAreaString + "Connection: ";
+		if(langRBAnd.getValue()){
+			dummyTextAreaString = dummyTextAreaString + "AND";
+		}else{
+			dummyTextAreaString = dummyTextAreaString + "OR";
+		}
+		
+		dummyTextArea.setText(dummyTextAreaString);
+		((HorizontalPanel)mainPanel.getWidget(1)).add(dummyTextArea);		
 	}
 }
 
