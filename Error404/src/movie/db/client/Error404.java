cff46a6c4665ee09f93995432b53c371012be1bb
rexorci <christoph.t.weber@gmail.com>
2015-11-01 14:13:40 +0100

    moved initialize stuff to functions

--- Error404/src/movie/db/client/Error404.java
+++ Error404/src/movie/db/client/Error404.java
@@ -3,26 +3,14 @@ package movie.db.client;
 
 /* a small step for software engineering, a huge step for error404! */
 //blablablatestestest ich bin ned kreativ
-import movie.db.shared.FieldVerifier;
+import java.util.ArrayList;
 
-import com.google.gwt.aria.client.ListboxRole;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
@@ -37,54 +25,44 @@ public class Error404 implements EntryPoint {
 	/**
 	 * This is the entry point method.
 	 */
-	private VerticalPanel mainPanel = new VerticalPanel();
-	
-	private HorizontalPanel selectionPanel = new HorizontalPanel();
-	private HorizontalPanel timebarPanel = new HorizontalPanel();
-	private HorizontalPanel worldmapPanel = new HorizontalPanel();
-	
-	private FlexTable selectionCriteriaTable = new FlexTable();
-	private Label genreLabel = new Label(); 
-	private Label langLabel = new Label(); 
-	private Label formatLabel = new Label(); 
-	private Button showButton = new Button();
-	
-	private ListBox genreDropdown = new ListBox();
-	private ListBox langDropdown = new ListBox();
-	private ListBox formatDropdown = new ListBox();
+
 	
 	public void onModuleLoad() {
-		genreLabel.setText("Genre:");
-		langLabel.setText("Language:");
-		formatLabel.setText("Format:");
-		showButton.setText("Show Results");
+		initializePanels();
+	}
+	
+	private void initializePanels(){
+		VerticalPanel mainPanel = new VerticalPanel();
 		
-		genreDropdown.addItem("Romance");	
-		genreDropdown.addItem("Comedy");
-		genreDropdown.addItem("Action");
-		genreDropdown.addItem("Horror");
-		genreDropdown.setVisibleItemCount(1);
+		HorizontalPanel selectionPanel = new HorizontalPanel();
+		HorizontalPanel timebarPanel = new HorizontalPanel();
+		HorizontalPanel worldmapPanel = new HorizontalPanel();
 		
-		langDropdown.addItem("English");	
-		langDropdown.addItem("German");	
-		langDropdown.addItem("Suahili");	
-		langDropdown.addItem("Klingonian");	
-		langDropdown.setVisibleItemCount(1);
-		
-		formatDropdown.addItem("Heatmap");	
-		formatDropdown.addItem("Piechart");	
-		formatDropdown.addItem("Barchart");	
-		formatDropdown.setVisibleItemCount(1);
-		
-		selectionCriteriaTable.setWidget(0,0,genreLabel);
-		selectionCriteriaTable.setWidget(0,1,genreDropdown);
-		selectionCriteriaTable.setWidget(1,0,langLabel);
-		selectionCriteriaTable.setWidget(1,1,langDropdown);
-		selectionCriteriaTable.setWidget(2,0,formatLabel);
-		selectionCriteriaTable.setWidget(2,1,formatDropdown);
-		selectionCriteriaTable.setWidget(3,0,showButton);
-		
+		FlexTable selectionCriteriaTable = new FlexTable();
+		Button showOnWorldmapButton = new Button();
 
+		ListBox genreDropdown = new ListBox();
+		
+		ListBox countryDropdown = new ListBox();
+		ListBox langDropdown = new ListBox();
+		ListBox formatDropdown = new ListBox();
+				
+		showOnWorldmapButton.setText("Show on Worldmap");		
+		genreDropdown.setMultipleSelect(true);
+		initializeGenres(genreDropdown);
+		initializeCountries(countryDropdown);
+		initializeLanguages(langDropdown);
+		initializeFormats(formatDropdown);		
+		
+		selectionCriteriaTable.setText(0,0,"Genre:");
+		selectionCriteriaTable.setWidget(0,1,genreDropdown);
+		selectionCriteriaTable.setText(1,0,"Country:");
+		selectionCriteriaTable.setWidget(1,1,countryDropdown);
+		selectionCriteriaTable.setText(2,0,"Language:");
+		selectionCriteriaTable.setWidget(2,1,langDropdown);
+		selectionCriteriaTable.setText(3,0,"Output Format:");
+		selectionCriteriaTable.setWidget(3,1,formatDropdown);
+		selectionCriteriaTable.setWidget(4,0,showOnWorldmapButton);
 		
 		selectionPanel.add(selectionCriteriaTable);
 		
@@ -102,6 +80,37 @@ public class Error404 implements EntryPoint {
 		
 		// Associate the Main panel with the HTML host page.
 		RootPanel.get("mainPage").add(mainPanel);
+	}
+	
+	private void initializeGenres(ListBox genreDropdown){		
+		genreDropdown.addItem("Romance");	
+		genreDropdown.addItem("Comedy");
+		genreDropdown.addItem("Action");
+		genreDropdown.addItem("Horror");
+		genreDropdown.addItem("Thriller");
+		genreDropdown.addItem("Science Fiction");
+		genreDropdown.addItem("Adventure");
+		genreDropdown.addItem("Supernatural");
+		genreDropdown.setVisibleItemCount(5);
+	}
+	private void initializeCountries(ListBox countryDropdown){
+		countryDropdown.addItem("United States");
+		countryDropdown.addItem("Switzerland");
+		countryDropdown.addItem("DeathStar");	
+		countryDropdown.setVisibleItemCount(1);
+	}
+	private void initializeLanguages(ListBox langDropdown){
+		langDropdown.addItem("English");	
+		langDropdown.addItem("German");	
+		langDropdown.addItem("Suahili");	
+		langDropdown.addItem("Klingonian");	
+		langDropdown.setVisibleItemCount(1);
+	}
+	private void initializeFormats(ListBox formatDropdown){
+		formatDropdown.addItem("Heatmap");	
+		formatDropdown.addItem("Piechart");	
+		formatDropdown.addItem("Barchart");	
+		formatDropdown.setVisibleItemCount(1);
 	}
 }
 
